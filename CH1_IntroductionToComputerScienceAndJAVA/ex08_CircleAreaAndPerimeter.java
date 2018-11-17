83f92fd88c9546c47297e82c169ea566d8f04604
Olecika Darii <olealea001@gmail.com>
2018-11-17 10:41:06 +0200

    update2

--- CH1_IntroductionToComputerScienceAndJAVA/ex08_CircleAreaAndPerimeter.java
+++ CH1_IntroductionToComputerScienceAndJAVA/ex08_CircleAreaAndPerimeter.java
@@ -1,10 +1,10 @@
-package Ch1_IntroductionCompsProgsJAVA;
+package CH1_IntroductionToComputerScienceAndJAVA;
 
 /** (Area and perimeter of a circle) Write a program that displays the area and perimeter
  of a circle that has a radius of 5.5 using the following formula:
     perimeter = 2 * radius * p
     area = radius * radius * p  */
-public class ex08 {
+public class ex08_CircleAreaAndPerimeter {
     public static void main(String[] args) {
         double radius = 5.5;
         double perimeter = 2 * radius * Math.PI;
